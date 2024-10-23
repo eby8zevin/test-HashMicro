ff7574dbc36921b5f21707e7bd94c66795d0302c
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2024-10-23 14:05:24 +0700

    feat(routes): config variable routes

---
+++ lib/routes/routes.dart
@@ -0,0 +1,11 @@
+part of 'pages.dart';
+
+abstract class Routes {
+
+  static const String splash = '/splash';
+  static const String dashboard = '/dashboard';
+  static const String geoTagging = '/geo_tagging';
+  static const String map = '/map';
+  static const String attendance = '/attendance';
+
+}
